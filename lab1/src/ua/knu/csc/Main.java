eef5f2b0ed33a478d6f46b874623568f14d2d4d6
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-17 15:46:08 +0200

    Use the MainWindow class

---
+++ lab1/src/ua/knu/csc/Main.java
@@ -0,0 +1,11 @@
+package ua.knu.csc;
+
+import ua.knu.csc.ui.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow("lab1");
+        mainWindow.setVisible(true);
+    }
+}
