ba56c69f73cbbcd433197f4c88579439af2014e2
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-18 00:35:32 +0200

    Create a new polygon

--- lab1/src/ua/knu/csc/Main.java
+++ lab1/src/ua/knu/csc/Main.java
@@ -2,19 +2,20 @@ package ua.knu.csc;
 
 import ua.knu.csc.ui.MainWindow;
 
-import java.awt.*;
+import java.awt.Point;
+import java.awt.Polygon;
 
 public class Main {
 
     public static void main(String[] args) {
-        Point originPoint = new Point(200, 300);
+        Point originPoint = new Point(50, 450);
 
-        int n = 4;
-        int[] x = {10, 10, 100, 100};
-        int[] y = {10, 50, 50, 10};
+        int n = 12;
+        int[] x = {10, 10, 25, 50, 80, 95, 130, 130, 170, 110, 50, 50};
+        int[] y = {60, 120, 90, 150, 150, 80, 80, 170, 80, 30, 45, 75};
 
         Polygon polygon = new Polygon(x, y, n);
-        Point point = new Point(100, 50);
+        Point point = new Point(0, 20);
 
         MainWindow mainWindow = new MainWindow("lab1", originPoint, polygon, point);
         mainWindow.setVisible(true);
