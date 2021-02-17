7684e13e1278991bf23b94fef71a3453a8c1aedf
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-17 18:50:13 +0200

    Draw the origin (point)

--- lab1/src/ua/knu/csc/Main.java
+++ lab1/src/ua/knu/csc/Main.java
@@ -7,12 +7,15 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
+        Point originPoint = new Point(20, 200);
+
         int[] x = {50, 150, 150, 50};
         int[] y = {50, 50, 100, 100};
         int n = 4;
+
         Polygon polygon = new Polygon(x, y, n);
 
-        MainWindow mainWindow = new MainWindow("lab1", polygon);
+        MainWindow mainWindow = new MainWindow("lab1", originPoint, polygon);
         mainWindow.setVisible(true);
     }
 }
--- lab1/src/ua/knu/csc/ui/MainWindow.java
+++ lab1/src/ua/knu/csc/ui/MainWindow.java
@@ -4,11 +4,21 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainWindow extends JFrame {
+    private final Point originPoint;
     private final Polygon polygon;
 
-    public MainWindow(String title, Polygon polygon) {
+    public MainWindow(String title, Point originPoint, Polygon polygon) throws NullPointerException {
         super(title);
 
+        if (originPoint == null) {
+            throw new NullPointerException("The specified origin is null.");
+        }
+
+        if (polygon == null) {
+            throw new NullPointerException("The specified polygon is null.");
+        }
+
+        this.originPoint = originPoint;
         this.polygon = polygon;
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -17,6 +27,18 @@ public class MainWindow extends JFrame {
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(screenSize.width / 2 - getSize().width / 2, screenSize.height / 2 - getSize().height / 2);
+    }
+
+    private void drawOriginPoint(Graphics graphics) {
+        Graphics2D graphics2D = (Graphics2D) graphics;
+
+        graphics2D.setColor(Color.RED);
+        graphics2D.setStroke(new BasicStroke(5f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER));
+
+        int x = originPoint.x;
+        int y = originPoint.y;
+
+        graphics2D.drawLine(x, y, x, y);
     }
 
     private void drawPolygon(Graphics graphics) {
@@ -31,6 +53,8 @@ public class MainWindow extends JFrame {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
+
+        drawOriginPoint(g);
         drawPolygon(g);
     }
 }
