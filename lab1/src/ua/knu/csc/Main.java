eb02ea9d99b21619d30a2668a9fff1eb116b5669
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-17 18:14:30 +0200

    Add the polygon to draw

--- lab1/src/ua/knu/csc/Main.java
+++ lab1/src/ua/knu/csc/Main.java
@@ -2,10 +2,17 @@ package ua.knu.csc;
 
 import ua.knu.csc.ui.MainWindow;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        MainWindow mainWindow = new MainWindow("lab1");
+        int[] x = {50, 150, 150, 50};
+        int[] y = {50, 50, 100, 100};
+        int n = 4;
+        Polygon polygon = new Polygon(x, y, n);
+
+        MainWindow mainWindow = new MainWindow("lab1", polygon);
         mainWindow.setVisible(true);
     }
 }
