0fb084417a7e4c6083d8a10519f70861f9a0f652
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-24 22:41:57 +0200

    Resize the polygon

--- lab1/src/ua/knu/csc/Main.java
+++ lab1/src/ua/knu/csc/Main.java
@@ -8,14 +8,14 @@ import java.awt.Polygon;
 public class Main {
 
     public static void main(String[] args) {
-        Point originPoint = new Point(50, 450);
+        Point originPoint = new Point(50, 500);
 
         int n = 12;
-        int[] x = {10, 10, 25, 50, 80, 95, 130, 130, 170, 110, 50, 50};
-        int[] y = {60, 120, 90, 150, 150, 80, 80, 170, 80, 30, 45, 75};
+        int[] x = {60, 60, 125, 200, 325, 360, 420, 420, 550, 390, 200, 200};
+        int[] y = {110, 290, 240, 370, 370, 240, 240, 370, 190, 70, 95, 150};
 
         Polygon polygon = new Polygon(x, y, n);
-        Point point = new Point(0, 20);
+        Point point = new Point(150, 230);
 
         MainWindow mainWindow = new MainWindow("lab1", originPoint, polygon, point);
         mainWindow.setVisible(true);
