44d3c6cc00a4cfa5415a8d0e0d5c53645f347b01
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-25 00:42:32 +0200

    Add examples: ordinary and special cases

--- lab1/src/ua/knu/csc/Main.java
+++ lab1/src/ua/knu/csc/Main.java
@@ -1,5 +1,7 @@
 package ua.knu.csc;
 
+import ua.knu.csc.core.EvenOddRule;
+
 import ua.knu.csc.ui.MainWindow;
 
 import java.awt.Point;
@@ -12,12 +14,26 @@ public class Main {
 
         int n = 12;
         int[] x = {60, 60, 125, 200, 325, 360, 420, 420, 550, 390, 200, 200};
-        int[] y = {110, 290, 240, 370, 370, 240, 240, 370, 190, 70, 95, 150};
+        int[] y = {110, 290, 240, 370, 370, 240, 240, 370, 150, 70, 95, 150};
 
         Polygon polygon = new Polygon(x, y, n);
-        Point point = new Point(150, 230);
+
+        // Ordinary cases
+        Point point = new Point(270, 330); // INSIDE
+        // Point point = new Point(100, 350); // OUTSIDE
+
+        // Special cases
+        // Point point = new Point(170, 370); // OUTSIDE
+        // Point point = new Point(20, 150); // OUTSIDE
+        // Point point = new Point(270, 240); // INSIDE
+        // Point point = new Point(100, 150); // INSIDE
+        // Point point = new Point(70, 240); // INSIDE
+        // Point point = new Point(390, 240); // BOUNDARY
+        // Point point = new Point(420, 370); // BOUNDARY
 
         MainWindow mainWindow = new MainWindow("lab1", originPoint, polygon, point);
         mainWindow.setVisible(true);
+
+        System.out.println(EvenOddRule.pointInPolygon(point, polygon));
     }
 }
