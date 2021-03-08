3d53963ee708e994937dc5a8ca30cd3f8a10bc98
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-09 00:30:54 +0200

    Use the MainWindow class

--- lab2/src/ua/knu/csc/Main.java
+++ lab2/src/ua/knu/csc/Main.java
@@ -1,8 +1,16 @@
 package ua.knu.csc;
 
+import ua.knu.csc.ui.MainWindow;
+import java.awt.Point;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Point originPoint = new Point(50, 500);
+
+        Point point = new Point(150, 150);
+
+        MainWindow mainWindow = new MainWindow("lab1", originPoint, point);
+        mainWindow.setVisible(true);
     }
 }
