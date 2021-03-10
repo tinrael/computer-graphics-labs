b49e460d08898811ed7026981c1f5b86317b105a
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-10 22:33:00 +0200

    UI code refactoring

--- lab2/src/ua/knu/csc/Main.java
+++ lab2/src/ua/knu/csc/Main.java
@@ -1,16 +1,38 @@
 package ua.knu.csc;
 
-import ua.knu.csc.ui.MainWindow;
+import ua.knu.csc.ui.DrawingPanel;
+
+import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 public class Main {
 
     public static void main(String[] args) {
-        Point originPoint = new Point(50, 500);
+        Point origin = new Point(50, 500);
 
-        Point point = new Point(150, 150);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI(origin);
+            }
+        });
+    }
 
-        MainWindow mainWindow = new MainWindow("lab1", originPoint, point);
+    private static void createAndShowGUI(Point origin) {
+        JFrame mainWindow = new JFrame("lab2");
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        mainWindow.add(new DrawingPanel(origin));
+
+        mainWindow.pack();
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        mainWindow.setLocation(screenSize.width / 2 - mainWindow.getSize().width / 2, screenSize.height / 2 - mainWindow.getSize().height / 2);
+
         mainWindow.setVisible(true);
     }
 }
