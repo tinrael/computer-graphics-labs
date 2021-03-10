fa55c02f5a5694b1a0af98d7abb195dd37a34eac
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-10 19:04:18 +0200

    Add the DirectedEdge class

---
+++ lab2/src/ua/knu/csc/entity/DirectedEdge.java
@@ -0,0 +1,39 @@
+package ua.knu.csc.entity;
+
+public class DirectedEdge {
+    private final Vertex from;
+    private final Vertex to;
+
+    private int weight;
+
+    public DirectedEdge(Vertex from, Vertex to, int weight) {
+        if (from == null) {
+            throw new NullPointerException("The specified vertex 'from' is null.");
+        }
+
+        if (to == null) {
+            throw new NullPointerException("The specified vertex 'to' is null.");
+        }
+
+        this.from = from;
+        this.to = to;
+
+        this.weight = weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Vertex getFrom() {
+        return from;
+    }
+
+    public Vertex getTo() {
+        return to;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
