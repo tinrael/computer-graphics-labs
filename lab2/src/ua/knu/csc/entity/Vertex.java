3763cd30d46fb563cec20c8b6ec5cd8195d7b42b
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-10 23:46:20 +0200

    Add the coordinates to the vertex

--- lab2/src/ua/knu/csc/entity/Vertex.java
+++ lab2/src/ua/knu/csc/entity/Vertex.java
@@ -3,15 +3,29 @@ package ua.knu.csc.entity;
 public class Vertex {
     private final String label;
 
-    public Vertex(String label) {
+    private int x;
+    private int y;
+
+    public Vertex(String label, int x, int y) {
         if (label == null) {
             throw new IllegalArgumentException("The specified label is null.");
         }
         this.label = label;
+        
+        this.x = x;
+        this.y = y;
     }
 
     public String getLabel() {
         return label;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     @Override
