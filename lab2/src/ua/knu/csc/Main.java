f31f998ab6c4abcc03ca39e0c85b10ab437554f8
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-17 00:52:10 +0200

    Update the edge-weighted digraph example

--- lab2/src/ua/knu/csc/Main.java
+++ lab2/src/ua/knu/csc/Main.java
@@ -21,21 +21,49 @@ public class Main {
 
         EdgeWeightedDigraph edgeWeightedDigraph = new EdgeWeightedDigraph();
 
-        Vertex v1 = new Vertex("v1", 20, 20);
-        Vertex v2 = new Vertex("v2", 20, 220);
-        Vertex v3 = new Vertex("v3", 420 ,20);
-        Vertex v4 = new Vertex("v3", 420, 220);
+        Vertex v1 = new Vertex("v1", 180, 90);
+        Vertex v2 = new Vertex("v2", 345, 141);
+        Vertex v3 = new Vertex("v3", 108 ,195);
+        Vertex v4 = new Vertex("v4", 216, 222);
+        Vertex v5 = new Vertex("v5", 327, 291);
+        Vertex v6 = new Vertex("v6", 252, 315);
+        Vertex v7 = new Vertex("v7", 309, 354);
+        Vertex v8 = new Vertex("v8", 63, 393);
+        Vertex v9 = new Vertex("v9", 228, 462);
 
         edgeWeightedDigraph.addVertex(v1);
         edgeWeightedDigraph.addVertex(v2);
         edgeWeightedDigraph.addVertex(v3);
         edgeWeightedDigraph.addVertex(v4);
+        edgeWeightedDigraph.addVertex(v5);
+        edgeWeightedDigraph.addVertex(v6);
+        edgeWeightedDigraph.addVertex(v7);
+        edgeWeightedDigraph.addVertex(v8);
+        edgeWeightedDigraph.addVertex(v9);
 
-        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v1, v3, 5));
-        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v1, v2, 5));
-        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v2, v3, 5));
-        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v3, v4, 5));
-        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v4, v2, 5));
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v1, v3, 0));
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v1, v4, 0));
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v1, v5, 0));
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v1, v2, 0));
+
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v2, v5, 0));
+
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v3, v8, 0));
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v3, v6, 0));
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v3, v4, 0));
+
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v4, v6, 0));
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v4, v5, 0));
+
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v5, v6, 0));
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v5, v7, 0));
+
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v6, v9, 0));
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v6, v7, 0));
+
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v7, v9, 0));
+
+        edgeWeightedDigraph.addDirectedEdge(new DirectedEdge(v8, v9, 0));
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
