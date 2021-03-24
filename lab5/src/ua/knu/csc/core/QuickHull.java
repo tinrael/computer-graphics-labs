be3ee102d19418a7ba27e8e079f6da12b86bb305
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-24 21:13:16 +0200

    Update the comment to the calculateTwiceAreaOfTriangle function

--- lab5/src/ua/knu/csc/core/QuickHull.java
+++ lab5/src/ua/knu/csc/core/QuickHull.java
@@ -106,7 +106,7 @@ public class QuickHull {
         return PointAndLineLocationType.BETWEEN;
     }
 
-    // Calculates twice the area of a triangle.
+    // Calculates unsigned (>= 0) twice the area of a triangle.
     public static int calculateTwiceAreaOfTriangle(Point p1, Point p2, Point p3) {
         return Math.abs((p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y));
     }
