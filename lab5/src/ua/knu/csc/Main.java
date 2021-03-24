2b36da6a31c2f1d3b4af5bd68741db8facc42f2b
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-24 17:56:01 +0200

    Update the Main to create and show GUI

--- lab5/src/ua/knu/csc/Main.java
+++ lab5/src/ua/knu/csc/Main.java
@@ -1,41 +1,56 @@
 package ua.knu.csc;
 
 import ua.knu.csc.core.QuickHull;
+import ua.knu.csc.ui.DrawingPanel;
 
-import java.awt.Point;
-
-import java.util.Set;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Set;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 public class Main {
 
     public static void main(String[] args) {
-        Set<Point> points1 = new HashSet<>(); // Convex hull: (0, 0) (0, 3) (3, 1) (4, 4).
-        points1.add(new Point(0, 3));
-        points1.add(new Point(1, 1));
-        points1.add(new Point(2, 2));
-        points1.add(new Point(4, 4));
-        points1.add(new Point(0, 0));
-        points1.add(new Point(1, 2));
-        points1.add(new Point(3, 1));
-        points1.add(new Point(3, 3));
+        Point origin = new Point(50, 500);
 
-        Set<Point> points2 = new HashSet<>(); // Convex hull: (-4, 0), (5, 0), (0, -6), (0, 4).
-        points2.add(new Point(0, 0));
-        points2.add(new Point(0, 4));
-        points2.add(new Point(-4, 0));
-        points2.add(new Point(5, 0));
-        points2.add(new Point(0, -6));
-        points2.add(new Point(1, 0));
+        Set<Point> points = new HashSet<>();
+        points.add(new Point(60, 110));
+        points.add(new Point(150, 40));
+        points.add(new Point(300, 210));
+        points.add(new Point(40, 300));
+        points.add(new Point(50, 50));
+        points.add(new Point(85, 90));
+        points.add(new Point(170, 210));
+        points.add(new Point(150, 180));
 
-        Set<Point> points3 = new HashSet<>(); // Convex hull: (6, 7), (7, 11), (11, 10), (8,8)
-        points3.add(new Point(6, 7));
-        points3.add(new Point(8, 8));
-        points3.add(new Point(11, 10));
-        points3.add(new Point(7, 11));
+        ArrayList<Point> convexHullPoints = QuickHull.findConvexHull(points);
+        System.out.println(convexHullPoints);
 
-        System.out.println(QuickHull.findConvexHull(points1));
-        System.out.println(QuickHull.findConvexHull(points2));
-        System.out.println(QuickHull.findConvexHull(points3));
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI(origin, points);
+            }
+        });
+    }
+
+    private static void createAndShowGUI(Point origin, Set<Point> points) {
+        JFrame mainWindow = new JFrame("lab5");
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        mainWindow.add(new DrawingPanel(origin, points));
+
+        mainWindow.pack();
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        mainWindow.setLocation(screenSize.width / 2 - mainWindow.getSize().width / 2, screenSize.height / 2 - mainWindow.getSize().height / 2);
+
+        mainWindow.setVisible(true);
     }
 }
