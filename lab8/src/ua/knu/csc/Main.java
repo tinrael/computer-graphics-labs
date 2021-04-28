4c448d5b5aff130001c7150676eaff5ba9aed3cf
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-04-28 23:03:34 +0300

    Create and show GUI

--- lab8/src/ua/knu/csc/Main.java
+++ lab8/src/ua/knu/csc/Main.java
@@ -1,8 +1,48 @@
 package ua.knu.csc;
 
+import ua.knu.csc.core.ConvexHull;
+import ua.knu.csc.ui.DrawingPanel;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Point origin = new Point(50, 500);
+
+        List<Point> simplePolygon = new ArrayList<>();
+
+        simplePolygon.add(new Point(60, 110));
+
+        List<Point> convexHull = ConvexHull.findConvexHullOfSimplePolygon(simplePolygon);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI(origin, simplePolygon, convexHull);
+            }
+        });
+    }
+
+    private static void createAndShowGUI(Point origin, List<Point> simplePolygon, List<Point> convexHull) {
+        JFrame mainWindow = new JFrame("lab8");
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        mainWindow.add(new DrawingPanel(origin, simplePolygon, convexHull));
+
+        mainWindow.pack();
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        mainWindow.setLocation(screenSize.width / 2 - mainWindow.getSize().width / 2, screenSize.height / 2 - mainWindow.getSize().height / 2);
+
+        mainWindow.setVisible(true);
     }
 }
